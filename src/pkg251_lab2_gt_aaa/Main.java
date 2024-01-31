8166e9b8dc728457695fea0c40ff600881544aaf
Noura <naalmoubarak@stu.kau.edu.sa>
2024-01-31 11:28:31 +0300

    simple print statement

---
+++ src/pkg251_lab2_gt_aaa/Main.java
@@ -0,0 +1,12 @@
+
+package pkg251_lab2_gt_aaa;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello github community");
+    }
+    
+}
